d64d805565db69aa2ae85ed3194bc8f8bf9b56dc
Hanks <zhanghan.me@gmail.com>
2017-02-15 19:42:46 +0800

    add matrix examples

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -16,38 +16,31 @@ double* mul (double *A, double *B) {
   return C;
 }
 
-// double determinant(double **a, int n)
-// {
-//    int i,j,j1,j2;
-//    double det = 0;
-//    double **m = NULL;
-//
-//    if (n < 1) { /* Error */
-//
-//    } else if (n == 1) { /* Shouldn't get used */
-//       det = a[0][0];
-//    } else if (n == 2) {
-//       det = a[0][0] * a[1][1] - a[1][0] * a[0][1];
-//    } else {
-//       det = 0;
-//       for (j1=0;j1<n;j1++) {
-//          m = malloc((n-1)*sizeof(double *));
-//          for (i=0;i<n-1;i++)
-//             m[i] = malloc((n-1)*sizeof(double));
-//          for (i=1;i<n;i++) {
-//             j2 = 0;
-//             for (j=0;j<n;j++) {
-//                if (j == j1)
-//                   continue;
-//                m[i-1][j2] = a[i][j];
-//                j2++;
-//             }
-//          }
-//          det += pow(-1.0,1.0+j1+1.0) * a[0][j1] * determinant(m, n-1);
-//          for (i=0;i<n-1;i++)
-//             free(m[i]);
-//          free(m);
-//       }
-//    }
-//    return(det);
-// }
+double det(double *A, int n) {
+   int i,j,j1,j2;
+   double determinant = 0;
+   double *M = NULL;
+
+   if (n == 1) { /* Shouldn't get used */
+      determinant = A[0];
+   } else if (n == 2) {
+      determinant = A[0] * A[3] - A[1] * A[2];
+   } else {
+      determinant = 0;
+      for (j1 = 0; j1 < n; j1++) {
+         M = malloc((n-1)*(n-1)*sizeof(double *));
+         for (i = 1; i < n; i++) {
+            j2 = 0;
+            for (j = 0; j < n; j++) {
+               if (j == j1) continue;
+               M[(i-1)*(n-1) + j2] = A[i*n + j];
+               j2++;
+            }
+         }
+         determinant += ((j1%2 == 0) ? 1 : -1) * A[j1] * det(M, n-1);
+         free(M);
+      }
+   }
+
+   return(determinant);
+}
