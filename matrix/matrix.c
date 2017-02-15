75cb3fab0f9392378feee0da1709914b73e6ae42
Hanks <zhanghan.me@gmail.com>
2017-02-15 19:51:55 +0800

    add det function

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -17,30 +17,30 @@ double* mul (double *A, double *B) {
 }
 
 double det(double *A, int n) {
-   int i,j,j1,j2;
-   double determinant = 0;
-   double *M = NULL;
+  int i,j,j1,j2;
+  double determinant = 0;
+  double *M = NULL;
 
-   if (n == 1) { /* Shouldn't get used */
-      determinant = A[0];
-   } else if (n == 2) {
-      determinant = A[0] * A[3] - A[1] * A[2];
-   } else {
-      determinant = 0;
-      for (j1 = 0; j1 < n; j1++) {
-         M = malloc((n-1)*(n-1)*sizeof(double *));
-         for (i = 1; i < n; i++) {
-            j2 = 0;
-            for (j = 0; j < n; j++) {
-               if (j == j1) continue;
-               M[(i-1)*(n-1) + j2] = A[i*n + j];
-               j2++;
-            }
-         }
-         determinant += ((j1%2 == 0) ? 1 : -1) * A[j1] * det(M, n-1);
-         free(M);
+  if (n == 1) {
+    determinant = A[0];
+  } else if (n == 2) {
+    determinant = A[0] * A[3] - A[1] * A[2];
+  } else {
+    determinant = 0;
+    for (j1 = 0; j1 < n; j1++) {
+      M = malloc((n-1)*(n-1)*sizeof(double *));
+      for (i = 1; i < n; i++) {
+        j2 = 0;
+        for (j = 0; j < n; j++) {
+          if (j == j1) continue;
+          M[(i-1)*(n-1) + j2] = A[i*n + j];
+          j2++;
+        }
       }
-   }
+      determinant += ((j1%2 == 0) ? 1 : -1) * A[j1] * det(M, n-1);
+      free(M);
+    }
+  }
 
-   return(determinant);
+  return(determinant);
 }
--- matrix/matrix.js
+++ matrix/matrix.js
@@ -13,5 +13,31 @@ function () {
     return C;
   }
 
-  return { mul }
+  function det (A, n = LENGTH) {
+    let determinant = 0
+
+    if (n == 1) {
+      determinant = A[0]
+    } else if (n == 2) {
+      determinant = A[0] * A[3] - A[1] * A[2]
+    } else {
+      determinant = 0
+      for (let x = 0; x < n; x++) {
+        const M = new Float32Array((n-1)*(n-1))
+        for (let i = 1; i < n; i++) {
+          let y = 0
+          for (let j = 0; j < n; j++) {
+            if (j == x) continue
+            M[(i-1)*(n-1) + y] = A[i*n + j]
+            y++
+          }
+        }
+        determinant += ((x%2 == 0) ? 1 : -1) * A[x] * det(M, n-1)
+      }
+    }
+
+    return determinant
+  }
+
+  return { mul, det }
 }
