831da82cf9ed22f44541ba37775b007db226652f
Hanks <zhanghan.me@gmail.com>
2017-02-15 19:47:58 +0800

    add mul function

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,3 +1,17 @@
 function () {
+  const LENGTH = 2;
 
+  function mul (A, B) {
+    const C = new Float32Array(LENGTH * LENGTH)
+    for (let i = 0; i < LENGTH; ++i) {
+      for (let j = 0; j < LENGTH; ++j) {
+        for (let k = 0; k < LENGTH; ++k) {
+          C[i*LENGTH + j] += A[i*LENGTH + k] * B[j + k*LENGTH];
+        }
+      }
+    }
+    return C;
+  }
+
+  return { mul }
 }
