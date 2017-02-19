34bbd624cba10a7f43ca90d21dc0f281542b4df9
Hanks <zhanghan.me@gmail.com>
2017-02-19 17:21:25 +0800

    add standalone examples

---
+++ standalone.js
@@ -0,0 +1,14 @@
+WebAssembly.compile(new Uint8Array(`
+  00 61 73 6d  0d 00 00 00  01 0c 02 60  02 7f 7f 01
+  7f 60 01 7f  01 7f 03 03  02 00 01 07  10 02 03 61
+  64 64 00 00  06 73 71 75  61 72 65 00  01 0a 13 02
+  08 00 20 00  20 01 6a 0f  0b 08 00 20  00 20 00 6c
+  0f 0b`.trim().split(/[\s\r\n]+/g).map(str => parseInt(str, 16))
+)).then(module => {
+  const instance = new WebAssembly.Instance(module)
+  const { add, square } = instance.exports
+
+  console.log('2 + 4 =', add(2, 4))
+  console.log('3^2 =', square(3))
+  console.log('(2 + 5)^2 =', square(add(2 + 5)))
+})
