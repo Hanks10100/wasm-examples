e6fdaf50b7d2bfef370a9ff1b7018681eadcc3e9
Hanks <zhanghan.me@gmail.com>
2017-03-17 18:03:16 +0800

    update cpp example

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 emcc math.c -Os -s WASM=1 -s SIDE_MODULE=1 -o math.wasm
+hexdump -ve '8/1 "%02x " "\n"' math.wasm
--- cpp/math.c
+++ cpp/math.c
@@ -1,7 +1,3 @@
-int add (int x, int y) {
-  return x + y;
-}
-
 int square (int x) {
   return x * x;
 }
